6fd4ead097a65c45c392fbbf4f15255ad5fcf067
Daniel Sanche <d.sanche14@gmail.com>
2020-05-04 20:54:22 -0700

    cleaned up yolo code

--- yolo/detect.py
+++ yolo/detect.py
@@ -23,132 +23,104 @@ from PIL import Image
 
 # construct the argument parse and parse the arguments
 
-confthres = 0.3
-nmsthres = 0.1
-yolo_path = './'
+class YOLO:
+    def __init__(self):
+        self.confthres = 0.3
+        self.nmsthres = 0.1
 
-def get_labels(labels_path):
-    # load the COCO class labels our YOLO model was trained on
-    #labelsPath = os.path.sep.join([yolo_path, "yolo_v3/coco.names"])
-    lpath=os.path.sep.join([yolo_path, labels_path])
-    LABELS = open(lpath).read().strip().split("\n")
-    return LABELS
+        labelsPath="yolo_v3/coco.names"
+        weightsPath="yolo_v3/yolov3.weights"
+        cfgPath="yolo_v3/yolov3.cfg"
 
-def get_colors(LABELS):
-    # initialize a list of colors to represent each possible class label
-    np.random.seed(42)
-    COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),dtype="uint8")
-    return COLORS
+        self.LABELS = open(labelsPath).read().strip().split("\n")
 
-def get_weights(weights_path):
-    # derive the paths to the YOLO weights and model configuration
-    weightsPath = os.path.sep.join([yolo_path, weights_path])
-    return weightsPath
-
-def get_config(config_path):
-    configPath = os.path.sep.join([yolo_path, config_path])
-    return configPath
-
-def load_model(configpath,weightspath):
-    # load our YOLO object detector trained on COCO dataset (80 classes)
-    print("[INFO] loading YOLO from disk...")
-    net = cv2.dnn.readNetFromDarknet(configpath, weightspath)
-    return net
+        print("[INFO] loading YOLO from disk...")
+        self.net = cv2.dnn.readNetFromDarknet(cfgPath, weightsPath)
 
 
+    def get_predection(self, image):
+        (H, W) = image.shape[:2]
 
-def get_predection(image,net,LABELS,COLORS):
-    (H, W) = image.shape[:2]
+        # determine only the *output* layer names that we need from YOLO
+        ln = self.net.getLayerNames()
+        ln = [ln[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
 
-    # determine only the *output* layer names that we need from YOLO
-    ln = net.getLayerNames()
-    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+        # construct a blob from the input image and then perform a forward
+        # pass of the YOLO object detector, giving us our bounding boxes and
+        # associated probabilities
+        blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
+                                     swapRB=True, crop=False)
+        self.net.setInput(blob)
+        start = time.time()
+        layerOutputs = self.net.forward(ln)
+        print(layerOutputs)
+        end = time.time()
 
-    # construct a blob from the input image and then perform a forward
-    # pass of the YOLO object detector, giving us our bounding boxes and
-    # associated probabilities
-    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
-                                 swapRB=True, crop=False)
-    net.setInput(blob)
-    start = time.time()
-    layerOutputs = net.forward(ln)
-    print(layerOutputs)
-    end = time.time()
+        # show timing information on YOLO
+        print("[INFO] YOLO took {:.6f} seconds".format(end - start))
 
-    # show timing information on YOLO
-    print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+        # initialize our lists of detected bounding boxes, confidences, and
+        # class IDs, respectively
+        boxes = []
+        confidences = []
+        classIDs = []
 
-    # initialize our lists of detected bounding boxes, confidences, and
-    # class IDs, respectively
-    boxes = []
-    confidences = []
-    classIDs = []
+        # loop over each of the layer outputs
+        for output in layerOutputs:
+            # loop over each of the detections
+            for detection in output:
+                # extract the class ID and confidence (i.e., probability) of
+                # the current object detection
+                scores = detection[5:]
+                # print(scores)
+                classID = np.argmax(scores)
+                # print(classID)
+                confidence = scores[classID]
 
-    # loop over each of the layer outputs
-    for output in layerOutputs:
-        # loop over each of the detections
-        for detection in output:
-            # extract the class ID and confidence (i.e., probability) of
-            # the current object detection
-            scores = detection[5:]
-            # print(scores)
-            classID = np.argmax(scores)
-            # print(classID)
-            confidence = scores[classID]
+                # filter out weak predictions by ensuring the detected
+                # probability is greater than the minimum probability
+                if confidence > self.confthres:
+                    # scale the bounding box coordinates back relative to the
+                    # size of the image, keeping in mind that YOLO actually
+                    # returns the center (x, y)-coordinates of the bounding
+                    # box followed by the boxes' width and height
+                    box = detection[0:4] * np.array([W, H, W, H])
+                    (centerX, centerY, width, height) = box.astype("int")
 
-            # filter out weak predictions by ensuring the detected
-            # probability is greater than the minimum probability
-            if confidence > confthres:
-                # scale the bounding box coordinates back relative to the
-                # size of the image, keeping in mind that YOLO actually
-                # returns the center (x, y)-coordinates of the bounding
-                # box followed by the boxes' width and height
-                box = detection[0:4] * np.array([W, H, W, H])
-                (centerX, centerY, width, height) = box.astype("int")
+                    # use the center (x, y)-coordinates to derive the top and
+                    # and left corner of the bounding box
+                    x = int(centerX - (width / 2))
+                    y = int(centerY - (height / 2))
 
-                # use the center (x, y)-coordinates to derive the top and
-                # and left corner of the bounding box
-                x = int(centerX - (width / 2))
-                y = int(centerY - (height / 2))
+                    # update our list of bounding box coordinates, confidences,
+                    # and class IDs
+                    boxes.append([x, y, int(width), int(height)])
+                    confidences.append(float(confidence))
+                    classIDs.append(classID)
 
-                # update our list of bounding box coordinates, confidences,
-                # and class IDs
-                boxes.append([x, y, int(width), int(height)])
-                confidences.append(float(confidence))
-                classIDs.append(classID)
+        # apply non-maxima suppression to suppress weak, overlapping bounding
+        # boxes
+        idxs = cv2.dnn.NMSBoxes(boxes, confidences, self.confthres, self.nmsthres)
 
-    # apply non-maxima suppression to suppress weak, overlapping bounding
-    # boxes
-    idxs = cv2.dnn.NMSBoxes(boxes, confidences, confthres,
-                            nmsthres)
+        # ensure at least one detection exists
+        found_boxes = []
+        if len(idxs) > 0:
+            # loop over the indexes we are keeping
+            for i in idxs.flatten():
 
-    # ensure at least one detection exists
-    found_boxes = []
-    if len(idxs) > 0:
-        # loop over the indexes we are keeping
-        for i in idxs.flatten():
-
-            # extract the bounding box coordinates
-            (x, y) = (boxes[i][0], boxes[i][1])
-            (w, h) = (boxes[i][2], boxes[i][3])
-            this_box = {'width': w,
-                        'height':h,
-                        'x':x,
-                        'y':y,
-                        'class':LABELS[classIDs[i]],
-                        'confidence':confidences[i]}
-            found_boxes.append(this_box)
-    return found_boxes
+                # extract the bounding box coordinates
+                (x, y) = (boxes[i][0], boxes[i][1])
+                (w, h) = (boxes[i][2], boxes[i][3])
+                this_box = {'width': w,
+                            'height':h,
+                            'x':x,
+                            'y':y,
+                            'class':self.LABELS[classIDs[i]],
+                            'confidence':confidences[i]}
+                found_boxes.append(this_box)
+        return found_boxes
 
 
-labelsPath="yolo_v3/coco.names"
-cfgpath="yolo_v3/yolov3.cfg"
-wpath="yolo_v3/yolov3.weights"
-Lables=get_labels(labelsPath)
-CFG=get_config(cfgpath)
-Weights=get_weights(wpath)
-nets=load_model(CFG,Weights)
-Colors=get_colors(Lables)
 # Initialize the Flask application
 app = Flask(__name__)
 
@@ -162,9 +134,11 @@ def main():
     npimg=np.array(img)
     image=npimg.copy()
     image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-    out_dict=get_predection(image,nets,Lables,Colors)
+
+    out_dict = yolo.get_predection(image)
     return {'yolo':out_dict}
 
     # start flask app
 if __name__ == '__main__':
+    yolo = YOLO()
     app.run(debug=True, host='0.0.0.0')
